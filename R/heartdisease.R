0cfc34353cfb14715c744126733b80eabd0c7f0d
SANTONLAR <57811760+SANTONLA@users.noreply.github.com>
2022-07-14 21:18:03 +0200

    Update heartdisease.R

--- R/heartdisease.R
+++ R/heartdisease.R
@@ -1,51 +1,74 @@
-#' Heart disease dataset
-#'
-#' The dataset is to be used with a supervised classification ML model to classify heart disease.
-#' @docType data
-#' @keywords heart disease heart disease machine learning classification
-#' @format A data frame with 918 rows and 10 variables:
-#' \describe{
-#'   \item{Age}{age of the patient presenting with heart disease}
-#'   \item{Sex}{gender of the patient}
-#'   \item{RestingBP}{blood pressure for resting heart beat}
-#'   \item{Cholesterol}{Cholesterol reading}
-#'   \item{FastingBS}{blood sample of glucose after a patient fasts \url{https://www.diabetes.co.uk/diabetes_care/fasting-blood-sugar-levels.html}}
-#'   \item{RestingECG}{Resting echocardiography is an indicator of previous myocardial infarction e.g. heart attack}
-#'   \item{MaxHR}{Maximum heart rate}
-#'   \item{Angina}{chest pain caused by decreased flood flow \url{https://www.nhs.uk/conditions/angina/}}
-#'   \item{HeartPeakReading}{reading at the peak of the heart rate}
-#'   \item{HeartDisease}{the classification label of whether patient has heart disease or not}
-#'
-#' }
-#' @source Collected by Gary Hutson \email{hutsons-hacks@outlook.com}, Dec-2021
-#' @examples
-#' library(dplyr)
-#' library(ConfusionTableR)
-#' data(heartdisease)
-#'
-#' # Convert diabetes data to factor'
-#' hd <- heartdisease %>%
-#'  glimpse() %>%
-#'  mutate(HeartDisease = as.factor(HeartDisease))
-#' # Check that the label is now a factor
-#'  is.factor(hd$HeartDisease)
-#'  # Dummy encoding
-#' # Get categorical columns
-#' hd_cat <- hd  %>%
-#'  dplyr::select_if(is.character)
+#---
+Título: "Detección anomalías cardiacas"
+Autor: "Silvia Antón"
+Fecha: "`r Sys.Date()`"
+output: html_document
+---
+#Cargamos las librerías
 
-#'  # Dummy encode the categorical variables
-#'  # Specify the columns to encode
-#'  cols <- c("RestingECG", "Angina", "Sex")
-#'  # Dummy encode using dummy_encoder in ConfusionTableR package
-#'  coded <- ConfusionTableR::dummy_encoder(hd_cat, cols, remove_original = TRUE)
-#' coded <- coded %>%
-#'     select(RestingECG_ST, RestingECG_LVH, Angina=Angina_Y,
-#'     Sex=Sex_F)
-#' # Remove column names we have encoded from original data frame
-#' hd_one <- hd[,!names(hd) %in% cols]
-#' # Bind the numerical data on to the categorical data
-#' hd_final <- bind_cols(coded, hd_one)
-#' # Output the final encoded data frame for the ML task
-#' glimpse(hd_final)
-"heartdisease"
+```{r, message=FALSE, warning=FALSE, results="hide"}
+check.packages <- function(pkg){
+    new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+    if (length(new.pkg)) 
+        install.packages(new.pkg, dependencies = TRUE)
+    sapply(pkg, require, character.only = TRUE)
+}
+
+check.packages(c("knitr","ggplot2", "tidymodels", "MLDataR", "stringi", "dplyr", "tidyr","data.table","confuionTableR","Oddsplotty","RMarkdown","Kableextra","devtools"))
+```
+
+## R Markdown
+
+Esto es un documento de Markdown. Markdown es una sintáxis de formato simple para crear documentosHTML, PDF, and MS Word documents. PAra más detalles consulte <http://rmarkdown.rstudio.com>.
+
+Cuando haces click en el botón **Knit** se generará un documento que incluye tanto  includes el contendio como la salida de cualquier fragmento de código R incrustado dentro del documento.
+#Leemos los datos
+```{r}
+data("heartdisease")
+str(heartdisease)
+DFheartdisease<-heartdisease
+```
+
+``
+El dataset es para utilizar con modelos de clasificación de  ML spervisados para clasificar las enfermedades cardiacas.
+
+#'  Un data frame con 918 filas y 10 variables:
+#' \descripción: {
+#'   \item{Age}{edad del paciente}
+#'   \item{Sex}{genero del paciente}
+#'   \item{RestingBP}{presión arterial en descanso}
+#'   \item{Cholesterol}{lectura colesterol}
+#'   \item{FastingBS}{muestras de sangre de glucosas después de esfuerzo \url{https://www.diabetes.co.uk/diabetes_care/fasting-blood-sugar-levels.html}}
+#'   \item{RestingECG}{elecrocardiograma en descanso es un indicador de infartoe.g. heart attack}
+#'   \item{MaxHR}{máxima frecuencia cardiaca}
+#'   \item{Angina}{dolor de pecho causado por disminución del riego sanguineo\url{https://www.nhs.uk/conditions/angina/}}
+#'   \item{HeartPeakReading}{pico de frencuencia cardiaca}
+#'   \item{HeartDisease}{clasificación de si tiene enfermedad cardiaca o no}
+Preparamos los nombres de las columnas
+
+
+```{r}
+
+names <- c("Edad",
+           "Sexo",
+           "Presion_descanso",
+           "Colesterol",
+           "Fasting_BS",
+           "Resting_ECG",
+           "MAxHR",
+           "Angina",
+           "HeartPeakREading",
+           "HeartDisease")
+```
+
+#Aplicamos los nombres de columna al dataset
+
+```{r}
+colnames(DFheartdisease) <- names
+```
+
+#Echamos un vistazo para verificar que los nombres de las columnas se han cambiado
+
+```{r}
+DFheartdisease %>% glimpse()
+```
